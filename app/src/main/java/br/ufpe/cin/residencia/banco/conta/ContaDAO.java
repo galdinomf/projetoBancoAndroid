ad67192a754f3204fad78660c8b2ade79a61d2ef
Moacir Galdino <mfmgl@cin.ufpe.br>
2023-04-22 11:54:17 -0300

    feat: métodos do BD na interface e no repositorio
    ContaDAO incluindo as assinaturas dos métodos para atualizar,
    remover e buscar contas.
    ContaRepository o corpo dos métodos citados acima.

--- app/src/main/java/br/ufpe/cin/residencia/banco/conta/ContaDAO.java
+++ app/src/main/java/br/ufpe/cin/residencia/banco/conta/ContaDAO.java
@@ -2,9 +2,11 @@ package br.ufpe.cin.residencia.banco.conta;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -15,11 +17,21 @@ public interface ContaDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void adicionar(Conta c);
 
-    //TODO incluir métodos para atualizar conta e remover conta
+    @Update(onConflict=OnConflictStrategy.REPLACE)
+    void atualizar(Conta c);
+
+    @Delete
+    void remover(Conta c);
+
+    @Query("SELECT * FROM contas WHERE numero = :numeroConta")
+    Conta buscarPeloNumero(String numeroConta);
+
+    @Query("SELECT * FROM contas WHERE nomeCliente = :nomeCliente")
+    List<Conta> buscarPeloNome(String nomeCliente);
+
+    @Query("SELECT * FROM contas WHERE cpfCliente = :cpfCliente")
+    List<Conta> buscarPeloCPF(String cpfCliente);
 
     @Query("SELECT * FROM contas ORDER BY numero ASC")
     LiveData<List<Conta>> contas();
-
-    //TODO incluir métodos para buscar pelo (1) número da conta, (2) pelo nome e (3) pelo CPF do Cliente
-
 }
