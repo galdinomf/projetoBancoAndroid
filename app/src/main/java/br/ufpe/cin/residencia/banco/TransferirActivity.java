877e4848a43e504c186cc750aae6e85d5b75b432
Moacir Galdino <mfmgl@cin.ufpe.br>
2023-05-06 16:23:44 -0300

    fix: contas verificadas antes de transferência
    Contas de origem e destino sendo buscadas no banco de
    dados antes da operação de transferência, que só é
    realizada se ambas as contas já estiverem registradas
    no banco.

--- app/src/main/java/br/ufpe/cin/residencia/banco/TransferirActivity.java
+++ app/src/main/java/br/ufpe/cin/residencia/banco/TransferirActivity.java
@@ -1,6 +1,7 @@
 package br.ufpe.cin.residencia.banco;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,10 +10,14 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import br.ufpe.cin.residencia.banco.conta.Conta;
+
 //Ver anotações TODO no código
 public class TransferirActivity extends AppCompatActivity {
 
     BancoViewModel viewModel;
+    boolean contaOrigemExite = false;
+    boolean contaDestinoExite = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,37 @@ public class TransferirActivity extends AppCompatActivity {
         tipoOperacao.setText(R.string.txt_tipoOperacao);
         btnOperacao.setText(R.string.btn_Operacao);
 
+        viewModel.contas.observe(this, contas -> {
+            if (contas.get(0) == null)
+                return ;
+            for (Conta conta: contas){
+                if (conta.numero.equals(numeroContaOrigem.getText().toString())){
+                    contaOrigemExite = true;
+                }
+                if (conta.numero.equals(numeroContaDestino.getText().toString())){
+                    contaDestinoExite = true;
+                }
+            }
+        });
+
+        numeroContaOrigem.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    viewModel.buscarPeloNumero(numeroContaOrigem.getText().toString());
+                }
+            }
+        });
+
+        numeroContaDestino.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    viewModel.buscarPeloNumero(numeroContaDestino.getText().toString());
+                }
+            }
+        });
+
         btnOperacao.setOnClickListener(
                 v -> {
                     String numOrigem = numeroContaOrigem.getText().toString();
@@ -39,8 +75,16 @@ public class TransferirActivity extends AppCompatActivity {
                         Toast.makeText(this, R.string.tst_verificar_numero_origem, Toast.LENGTH_SHORT).show();
                         return;
                     }
+                    if (!contaOrigemExite) {
+                        Toast.makeText(this, "conta de origem errada", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
                     if (numDestino.length() != 3) {
                         Toast.makeText(this, R.string.tst_verificar_numero_destino, Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    if (!contaDestinoExite) {
+                        Toast.makeText(this, "conta de destino errada", Toast.LENGTH_SHORT).show();
                         return;
                     }
                     String valorDigitado = valorOperacao.getText().toString();
